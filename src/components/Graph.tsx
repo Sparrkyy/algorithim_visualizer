a1888e3af6c8dddc475aba9a5a29e741561d0125
useStateEthan <ethancampbellhodges@gmail.com>
2021-07-04 12:38:28 -0400

    Updated readme and more set up

---
+++ src/components/Graph.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { basicGraph } from '../graphs/basicGraph'
+
+const Graph = () => {
+    return (
+        <div style={{display: "flex", justifyContent: 'center', alignItems: 'center'}} className='graph_meta_container'>
+            <div style={{display: "flex"}} className="graph_container">
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Graph
