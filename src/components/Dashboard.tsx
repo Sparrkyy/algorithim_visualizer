f0da464df2870d9393ad0b7e7a1396e33ee4ed5d
useStateEthan <ethancampbellhodges@gmail.com>
2021-08-03 16:54:48 -0400

    added the menu dropdown to pick a graph type, but it doesn't reset right, maybe just ignore that for now?

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FC, useRef, useState } from "react";
 import OptionsBar from "./OptionsBar";
 import GraphComponent, { generateGraph } from "./Graph";
-import { NodeCords } from "../types";
+import { NodeCords, GraphUnitTypes, graphTypes } from "../types";
 import { useEffect } from "react";
 import "../css/Dashboard.css";
 
@@ -18,12 +18,48 @@ const Dashboard: FC = () => {
 	const [graph, setGraph] = useState(
 		generateGraph(startNodeRef.current, finishNodeRef.current, graphHeight, graphWidth)
 	);
+	const [graphType, setGraphType] = useState(graphTypes.NORMAL);
 
 	useEffect(() => {
-		const graphWidthPixels = 900;
+		const graphWidthPixels = 1000;
 		nodeDiameter.current = graphWidthPixels / graphWidth;
 		setGraph(generateGraph(startNodeRef.current, finishNodeRef.current, graphHeight, graphWidth));
 	}, [graphWidth, graphHeight]);
+
+	useEffect(() => {
+		switch (graphType) {
+			case graphTypes.NORMAL:
+				setGraph(generateGraph(startNodeRef.current, finishNodeRef.current, graphHeight, graphWidth));
+				break;
+			case graphTypes.HOLES:
+				setGraph((prev) => {
+					const dup = [...prev];
+					dup.forEach((row, ix) => {
+						row.forEach((node, iy) => {
+							if (dup[ix][iy].type === GraphUnitTypes.FINISH) {
+							} else if (dup[ix][iy].type === GraphUnitTypes.START) {
+							} else if (iy % 6 === 0 && ix % 6 === 0) {
+								dup[ix][iy].type = GraphUnitTypes.EMPTY_SPACE;
+								if (ix + 1 < dup.length) {
+									dup[ix + 1][iy].type = GraphUnitTypes.EMPTY_SPACE;
+								}
+								if (ix - 1 > 0) {
+									dup[ix - 1][iy].type = GraphUnitTypes.EMPTY_SPACE;
+								}
+								if (iy + 1 < dup[0].length) {
+									dup[ix][iy + 1].type = GraphUnitTypes.EMPTY_SPACE;
+								}
+								if (iy - 1 > 0) {
+									dup[ix][iy - 1].type = GraphUnitTypes.EMPTY_SPACE;
+								}
+							}
+						});
+					});
+					return dup;
+				});
+				break;
+		}
+	}, [graphType, graphHeight, graphWidth]);
 
 	return (
 		<div className='dashboard_container'>
@@ -40,6 +76,8 @@ const Dashboard: FC = () => {
 				GRAPH_WIDTH={graphWidth}
 				setGraphHeight={setGraphHeight}
 				setGraphWidth={setGraphWidth}
+				setGraphType={setGraphType}
+				graphType={graphType}
 			/>
 		</div>
 	);
--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,33 @@
 export type NodeCords = [row: number, col: number];
 
+export enum graphTypes {
+	NORMAL = "Normal",
+	HOLES = "Holes",
+}
+
 export enum GraphUnitTypes {
-  START = "start-node",
-  FINISH = "finish-node",
-  NODE = "normal-node",
-  NODE_IN_QUEUE = "node-in-queue",
-  LEFT_RIGHT_EDGE = "left-right-edge",
-  UP_DOWN_EDGE = "up-down-edge",
-  EMPTY_SPACE = "empty-space",
-  VISITED_NODE = "visited-node",
-  IN_SHORTEST_PATH_NODE = "shortest-path-node",
+	START = "start-node",
+	FINISH = "finish-node",
+	NODE = "normal-node",
+	NODE_IN_QUEUE = "node-in-queue",
+	LEFT_RIGHT_EDGE = "left-right-edge",
+	UP_DOWN_EDGE = "up-down-edge",
+	EMPTY_SPACE = "empty-space",
+	VISITED_NODE = "visited-node",
+	IN_SHORTEST_PATH_NODE = "shortest-path-node",
 }
 
 export type GraphUnit = {
-  type: GraphUnitTypes;
-  visited: boolean;
-  queued: boolean;
-  cords: NodeCords;
-  previous?: NodeCords;
+	type: GraphUnitTypes;
+	visited: boolean;
+	queued: boolean;
+	cords: NodeCords;
+	previous?: NodeCords;
 };
 
 export type GraphType = GraphUnit[][];
 
 export enum AlgoTypes {
-  DFS = "Depth First Search",
-  BFS = "Breadth First Search",
+	DFS = "Depth First Search",
+	BFS = "Breadth First Search",
 }
